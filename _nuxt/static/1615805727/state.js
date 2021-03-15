2799f698ff4a72128dd892c7d1ec9c825c6cea6f
Kirill Stytsenko <kirill@styts.com>
2021-03-15 11:55:28 +0100

    1c76fb0

---
+++ _nuxt/static/1615805727/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftz-guestbook\u002F_nuxt\u002Fstatic\u002F1615805727",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Ftz-guestbook\u002F",assetsPath:"\u002Ftz-guestbook\u002F_nuxt\u002F",cdnURL:a}}}}(null));
