1c76fb00b8acd9a60f1c9ecd9aa800b6182729de
Kirill Stytsenko <kirill@styts.com>
2021-03-15 11:23:08 +0100

    Fix nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,7 +52,6 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
     theme: {
       dark: false,
       themes: {
